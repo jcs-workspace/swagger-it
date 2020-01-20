b61e124800910e68e6901b07f9ad71fac70b18ad
jenchieh <jcs090218@gmail.com>
2020-01-20 17:45:56 +0800

    Add examples.

---
+++ examples/test.go
@@ -0,0 +1,9 @@
+/**
+ * $File: test.go $
+ * $Date: 2020-01-19 20:48:18 $
+ * $Revision: $
+ * $Creator: Jen-Chieh Shen $
+ * $Notice: See LICENSE.txt for modification and distribution information
+ *                   Copyright © 2020 by Shen, Jen-Chieh $
+ */
+
---
+++ examples/test.js
@@ -0,0 +1,35 @@
+/**
+ * $File: test.js $
+ * $Date: 2020-01-19 00:35:46 $
+ * $Revision: $
+ * $Creator: Jen-Chieh Shen $
+ * $Notice: See LICENSE.txt for modification and distribution information
+ *                   Copyright © 2020 by Shen, Jen-Chieh $
+ */
+
+"use strict";
+
+function addPet() {
+
+}
+
+/**
+ * @swagger
+ * @title Swagger Example API
+ * @version 1.0
+ * @description This is a sample server Petstore server.
+ * @termsOfService http://swagger.io/terms/
+ *
+ * @contact.name API Support
+ * @contact.url http://www.swagger.io/support
+ * @contact.email support@swagger.io
+ *
+ * @license.name Apache 2.0
+ * @license.url http://www.apache.org/licenses/LICENSE-2.0.html
+ *
+ * @host petstore.swagger.io
+ * @BasePath /v2
+ */
+function main() {
+
+}
