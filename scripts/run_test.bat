a6460a04ac85cd6da52200f93e89044c5df7e0f4
jenchieh <jcs090218@gmail.com>
2020-01-22 00:46:47 +0800

    Add script.

---
+++ scripts/run_test.bat
@@ -0,0 +1,13 @@
+@echo off
+:: ========================================================================
+:: $File: run_test.bat $
+:: $Date: 2020-01-21 21:27:20 $
+:: $Revision: $
+:: $Creator: Jen-Chieh Shen $
+:: $Notice: See LICENSE.txt for modification and distribution information
+::                   Copyright © 2020 by Shen, Jen-Chieh $
+:: ========================================================================
+
+cd ..
+
+python swagger-it.py -i ./
